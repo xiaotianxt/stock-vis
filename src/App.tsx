59f697431b967a47be824dd5de8cf1031ed5840c
xiaotianxt <tianyp@pku.edu.cn>
2023-01-10 18:13:11 +0800

    style(layout): update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import ControlPanel from "./ControlPanel";
 import StockChart from "./StockChart";
 import MessageBox from "./MessageBox";
 
 function App() {
   return (
     <>
+      <div className="fixed top-2 w-full text-center text-slate-100 text-xl font-bold p-2 bg-slate-900 opacity-70">
+        股票可视化 —— 房地产行业 20 年大事纪
+      </div>
       <StockChart />
-      <ControlPanel />
       <MessageBox />
     </>
   );
