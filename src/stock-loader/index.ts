076fd0084f40024aea0dd007d8723e272d30fbd8
xiaotianxt <tianyp@pku.edu.cn>
2023-01-10 13:17:51 +0800

    feat(stock-loader): new stock-loader with data alignment

--- src/stock-loader/index.ts
+++ src/stock-loader/index.ts
@@ -1,14 +1,48 @@
 import _ from "lodash";
-import { StockData, toStockData } from "./types";
+import { DataLoad, StockData, toStockData } from "./types";
 import { useEffect, useMemo, useState } from "react";
+import { Moment } from "moment";
 
-export const useDataLoader = (
-  tscode: string,
+export const useDataLoaders = (
+  tscodes: string[],
   step: number = 1,
   size: number = 50
 ) => {
+  const loaders = tscodes.map((tscode) => useDataLoader(tscode));
+  const startDates = useMemo(() => {
+    return loaders.map((loader) => loader.data?.[0]?.trade_date);
+  }, [loaders[0].data]);
+  const [date, setDate] = useState<Moment>();
+  const [slices, setSlices] = useState<StockData[][]>([]);
+  const [weeks, setWeeks] = useState<string[]>([]);
+
+  useEffect(() => {
+    setDate(_.min(startDates)?.subtract(size, "weeks")?.clone());
+  }, [startDates]);
+
+  const next = () => {
+    if (date === undefined) return;
+    const start = date.clone().add(step, "weeks");
+    const end = start.clone().add(size, "weeks");
+    const slices = loaders.map((loader, idx) => {
+      loader.next(start, end);
+      return loader.slice;
+    });
+    setDate(start);
+    setSlices(slices);
+    const weeks = [];
+    for (let date = start.clone(); date < end; date.add(1, "weeks")) {
+      weeks.push(date.format("YYYY-MM-DD"));
+    }
+    setWeeks(weeks);
+  };
+
+  return { slices, next, weeks };
+};
+
+export const useDataLoader = (tscode: string) => {
   const [data, setData] = useState<StockData[]>([]);
-  const [index, setIndex] = useState<number>(0);
+  const [slice, setSlice] = useState<StockData[]>([]);
 
   useEffect(() => {
     const url = "/data/" + tscode + ".csv";
@@ -19,16 +53,27 @@ export const useDataLoader = (
       });
   }, []);
 
-  const slice = useMemo<StockData[]>(
-    () => data.slice(index, index + size),
-    [data, index, size]
-  );
-
-  const next = () => {
-    setIndex((old) => old + step);
+  const next = (start: Moment, end: Moment) => {
+    const first = data?.[0]?.trade_date;
+    if (first === undefined) return;
+    const from = _.sortedIndexBy(
+      data,
+      { trade_date: start } as StockData,
+      (item) => item.trade_date
+    );
+    const to = _.sortedIndexBy(
+      data,
+      { trade_date: end } as StockData,
+      (item) => item.trade_date
+    );
+    setSlice(from == to ? [] : data.slice(from, to));
   };
 
-  return { next, slice, data };
+  return {
+    next,
+    slice,
+    data,
+  } as DataLoad;
 };
 
 function csvToObjectArray<ObjectType extends object>(
--- src/stock-loader/types.ts
+++ src/stock-loader/types.ts
@@ -3,22 +3,27 @@ import moment, { Moment } from "moment";
 
 export type StockData = {
   trade_date: Moment;
+  date: string;
   close: number;
   open: number;
   high: number;
   low: number;
-  vol: number;
-  pct_chg: number;
 };
 
 export function toStockData(header: string[], row: string[]): StockData {
   return {
-    trade_date: moment(row[header.indexOf("trade_date")], "YYYYMMDD"),
+    trade_date: moment(row[header.indexOf("trade_date")], "YYYY-MM-DD"),
+    date: row[header.indexOf("trade_date")],
     close: parseFloat(row[header.indexOf("close")]),
     open: parseFloat(row[header.indexOf("open")]),
     high: parseFloat(row[header.indexOf("high")]),
     low: parseFloat(row[header.indexOf("low")]),
-    vol: parseFloat(row[header.indexOf("vol")]),
-    pct_chg: parseFloat(row[header.indexOf("pct_chg")]),
   };
 }
+
+export type DataLoad = {
+  next: (start: Moment, end: Moment) => void;
+  slice: StockData[];
+  data: StockData[];
+  start: boolean;
+};
