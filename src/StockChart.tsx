7e5c9b912613c5a8006b6ad33e14c1b311d3826c
xiaotianxt <tianyp@pku.edu.cn>
2023-01-10 14:32:35 +0800

    feat(vis): add chart for price vis

--- src/StockChart.tsx
+++ src/StockChart.tsx
@@ -1,36 +1,51 @@
 import { useEffect, useMemo } from "react";
 import { ReactECharts } from "./ReactEcharts";
 import { EChartsOption } from "echarts";
-import { useDataLoader } from "./stock-loader";
+import { useDataLoaders } from "./stock-loader";
 
 export const Index = () => {
+  const files = ["out000002.SZ", "outSSEC", "outSZREI"];
+  const names = ["万达", "上证指数", "房地产指数"];
   // load data
-  const { slice, next } = useDataLoader("000002.SZ", 2, 100);
+  const { slices, next, weeks } = useDataLoaders(files, 1, 10);
   const option: EChartsOption = useMemo<EChartsOption>(() => {
+    const series: EChartsOption["series"] = slices.map((slice, idx) => {
+      const name = names[idx];
+      return {
+        name,
+        type: "line",
+        stack: "Total",
+        smooth: true,
+        data: [
+          ...Array(weeks.length - slice.length).fill(undefined),
+          ...slice.map((slice) => slice.close),
+        ],
+        yAxisIndex: idx === 0 ? 0 : 1,
+      };
+    });
+
     return {
       xAxis: {
         type: "category",
-        data: slice.map((d) => d.trade_date.format("YYYY-MM-DD")),
         splitLine: { show: false },
+        data: weeks,
       },
-      yAxis: {
-        type: "value",
-        splitLine: { show: false },
-      },
-      series: [
+      yAxis: [
         {
-          type: "candlestick",
-          data: slice.map((d) => [d.open, d.close, d.low, d.high]),
-          smooth: true,
-          showSymbol: false,
+          splitLine: { show: false },
+        },
+        {
+          splitLine: { show: false },
         },
       ],
+      series,
       tooltip: {
         trigger: "axis",
       },
       backgroundColor: "transparent",
+      animationEasing: "bounceInOut",
     };
-  }, [slice]);
+  }, [slices]);
 
   useEffect(() => {
     const interval = setInterval(() => {
